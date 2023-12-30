75b9aa5b640f223cb1e15d94d0d08ce8818c05be
Amal Mishra <amalmishraoff@gmail.com>
2023-12-30 12:16:31 +0530

    added diamond image
    add diamond image

--- I_Am_Rich/lib/main.dart
+++ I_Am_Rich/lib/main.dart
@@ -14,8 +14,7 @@ void main() {
         backgroundColor: Colors.blueGrey.shade900,
         body: const Center(
           child: Image(
-            image: NetworkImage(
-                'https://charts.ecmwf.int/streaming/20231226-0540/50/webp-worker-commands-558f87fd99-bbdvd-6fe5cac1a363ec1525f54343b6cc9fd8-VY9BEh.webp'),
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
